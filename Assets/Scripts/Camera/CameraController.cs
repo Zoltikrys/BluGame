286c6abdaa92ddb24f466306d8310426dc227397
Barrycetamol <terry.victor.dale@gmail.com>
2024-10-26 17:03:46 +0100

    Added additional camera controls when following a focal point

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -17,6 +17,19 @@ public class CameraController : MonoBehaviour
     public bool UseFocalPoint {get; set;} = true;
 
     [field: SerializeField]
+    [field: Tooltip("Should camera follow along with focal point?")]
+    public bool FollowFocalPoint{get; set;} = true;
+
+    [field: SerializeField]
+    [field: Tooltip("How far away from FocalPoint should we be?")]
+    public int Distance {get; set;}
+    
+    [field: SerializeField]
+    [field: Tooltip("How high should the camera be?")]
+    public int Height {get; set;}
+
+
+    [field: SerializeField]
     [field: Tooltip("The object the camera should be looking at")]
     public GameObject FocalPoint {get; set;}
 
@@ -24,10 +37,12 @@ public class CameraController : MonoBehaviour
     [field: SerializeField]
     public float CameraMoveSpeed{get; set; } = 1.0f;
 
+    private Vector3 DistanceFromFocalPoint{get; set;}
     /// <summary>
     /// List containing the camera presets found within a scene.
     /// </summary>
-    private List<Transform> CameraPositions = new List<Transform>();
+    private List<TransformData> CameraPositions = new List<TransformData>();
+
 
     /// <summary>
     /// The currently active camera index
@@ -39,14 +54,16 @@ public class CameraController : MonoBehaviour
     /// </summary>
     private int PrevCameraIndex = -1;
 
+    private int CameraRotation = 0;
+
     /// <summary>
     /// Contains the current lerp target for the camera. Used when shifting from 1 camera to another
     /// </summary>
-    private Transform CameraTargetPosition {get; set;}
+    public TransformData CameraTargetPosition;
 
     void Start()
     {
-        CameraTargetPosition = transform;
+
         RefreshPresetCameraPositions();
     }
 
@@ -56,8 +73,9 @@ public class CameraController : MonoBehaviour
     private void RefreshPresetCameraPositions(){
         CameraPositions.Clear();
         var cameraPositionsForlevel = GameObject.FindGameObjectWithTag("CameraPositions");
+
         foreach(Transform cameraPos in cameraPositionsForlevel.transform){
-            CameraPositions.Add(cameraPos.transform);
+            CameraPositions.Add(new TransformData(cameraPos));
         }
         Debug.Log($"Found {CameraPositions.Count} camera presets for scene");
     }
@@ -74,7 +92,14 @@ public class CameraController : MonoBehaviour
             }
         }
 
-        if(UseFocalPoint && FocalPoint != null) CameraTargetPosition.LookAt(FocalPoint.transform);
+        if(FollowFocalPoint && FocalPoint != null){ // Camera follow focal point
+            CameraTargetPosition.position = CalculateCameraPlacementFromFocalPoint();
+        } 
+
+        if(UseFocalPoint && FocalPoint != null) {  // Looks at focal point
+            Quaternion lookRotation = Quaternion.LookRotation(FocalPoint.transform.position - CameraTargetPosition.position);
+            CameraTargetPosition.rotation = lookRotation;
+        }
 
         transform.position = Vector3.Lerp(transform.position, CameraTargetPosition.position, Time.deltaTime * CameraMoveSpeed);
         transform.rotation = Quaternion.Lerp(transform.rotation, CameraTargetPosition.rotation, Time.deltaTime * CameraMoveSpeed);
@@ -82,19 +107,61 @@ public class CameraController : MonoBehaviour
     }
 
     /// <summary>
-    /// Handles inputs that control the camera. Currently uses Q and E to rotate between camera presets.
+    /// Handles camera rotation when not using presets. Currently only processes 90* camera movements
+    /// </summary>
+    /// <returns>A position to place the camera at</returns>
+    private Vector3 CalculateCameraPlacementFromFocalPoint(){
+        Vector3 cameraPosition = FocalPoint.transform.position;
+        Vector3 offsetDistance = new Vector3(0, Height, 0);
+
+
+        switch(CameraRotation){
+            case 3: offsetDistance.x = -Distance * 2;
+                    offsetDistance.z = 0;
+                    break;
+            case 2: offsetDistance.x = 0;
+                    offsetDistance.z = Distance;
+                    break;
+            case 1: offsetDistance.x = Distance;
+                    offsetDistance.z = 0;
+                    break;
+            case 0: offsetDistance.x = 0;
+                    offsetDistance.z = -Distance;
+                    break;
+        }
+        return cameraPosition + offsetDistance;
+    }
+
+    /// <summary>
+    /// Handles inputs that control the camera. Currently uses Q and E to rotate between camera presets or rotate the camera 90*
     /// </summary>
     private void HandleKeyPress()
     {
         if(Input.GetKeyUp(KeyCode.Q)){
-            PrevCameraIndex = CurrentCameraIndex;
-            CurrentCameraIndex -= 1;
-            if(CurrentCameraIndex < 0) CurrentCameraIndex = CameraPositions.Count - 1;
+            if(FollowFocalPoint){
+                CameraRotation -= 1;
+                if(CameraRotation < 0) CameraRotation = 3;
+            }
+            else{
+                PrevCameraIndex = CurrentCameraIndex;
+                CurrentCameraIndex -= 1;
+                if(CurrentCameraIndex < 0) CurrentCameraIndex = CameraPositions.Count - 1;
+            }
+            Debug.Log($"Camera Rotation {CameraRotation}");
+
         }
         if(Input.GetKeyUp(KeyCode.E)){
-            PrevCameraIndex = CurrentCameraIndex;
-            CurrentCameraIndex += 1;
-            if(CurrentCameraIndex >= CameraPositions.Count) CurrentCameraIndex = 0;
+            if(FollowFocalPoint){
+                CameraRotation += 1;
+                if(CameraRotation > 3) CameraRotation = 0;
+            }
+            else{
+                PrevCameraIndex = CurrentCameraIndex;
+                CurrentCameraIndex += 1;
+                if(CurrentCameraIndex >= CameraPositions.Count) CurrentCameraIndex = 0;
+            }
+            Debug.Log($"Camera Rotation {CameraRotation}");
+
         }
     }
 }
