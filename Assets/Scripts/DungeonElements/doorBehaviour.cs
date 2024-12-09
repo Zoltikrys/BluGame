d319d5feb59bcf60d047381884a90dde484f8c9b
logandando <logandando2002@gmail.com>
2024-12-09 01:40:42 +0000

    Cel Shading Armageddon
    A LOT of changes and experimenting. Testing ~3 different cel shaders and their effects. A lot of this needs to be culled but I haven't gotten round to sorting through all of it. Also made a start on sorting out the level meshes but this will all need to be reevaluated. Will sort out the visual style and then fully prepare everything necessary. Apologies for the massive update.

--- Assets/Scripts/DungeonElements/doorBehaviour.cs
+++ Assets/Scripts/DungeonElements/doorBehaviour.cs
@@ -13,34 +13,36 @@ public class doorBehaviour : MonoBehaviour
     private GameObject rightDoor;
     [SerializeField] public GameObject collider;
 
-
+    [SerializeField]
+    private Animator animator = null;
     private DoorTransition doorTransition;
 
 
     private void Start()
     {
-        if(doorOpenFlag == false) {
-            CloseDoor();
-        }
-        else {
+        animator = GetComponent<Animator>();
+        if (doorOpenFlag == true) {
             OpenDoor();
         }
+
         TryGetComponent<DoorTransition>(out doorTransition);
     }
 
     public void OpenDoor()
     {
-        leftDoor.SetActive(false);
-        rightDoor.SetActive(false);
+        animator.Play("DoorOpen", 0, 0.0f);
+        //leftDoor.SetActive(false);
+        //rightDoor.SetActive(false);
         doorOpenFlag = true;
         collider.GetComponent<Collider>().enabled = true;
     }
 
     public void CloseDoor()
     {
+        animator.Play("DoorClose", 0, 0.0f);
         Debug.Log("this door should be closed");
-        leftDoor.SetActive(true);
-        rightDoor.SetActive(true);
+        //leftDoor.SetActive(true);
+        //rightDoor.SetActive(true);
         doorOpenFlag = false;
         collider.GetComponent<Collider>().enabled = false;
     }
