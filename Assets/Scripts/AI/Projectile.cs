e2936fe55dd136ba281d4ae9f8aafb86c1bedb92
Christopher Bridges <162894664+Zoltikrys@users.noreply.github.com>
2025-01-09 17:49:33 +0000

    Merged stationary enemy

---
+++ Assets/Scripts/AI/Projectile.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed = 1f;
+    public float damage = 10f;
+    public float lifeTime = 5000f;
+
+    void Start()
+    {
+        Destroy(gameObject, lifeTime); // Destroy after lifeTime seconds
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // Assumes player is tagged as "Player"
+        {
+            Debug.Log("Hit BLU");
+            HealthManager healthMan = other.gameObject.GetComponent<HealthManager>(); // damage player
+            healthMan.Damage();
+            //GetComponent<HealthManager>().Damage();
+            //Destroy(gameObject); // Destroy on collision
+        }
+
+        Debug.Log("Collided with " + other.gameObject.name); //for debugging
+
+        Destroy(gameObject); // Destroy on collision
+    }
+}
