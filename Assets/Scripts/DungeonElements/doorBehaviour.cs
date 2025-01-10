748c0d0dc89879877b3000251c9efbdb850e1779
Barrycetamol <59121217+Barrycetamol@users.noreply.github.com>
2025-01-10 15:45:23 +0000

    Maybe door fixed?

--- Assets/Scripts/DungeonElements/doorBehaviour.cs
+++ Assets/Scripts/DungeonElements/doorBehaviour.cs
@@ -2,24 +2,26 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class doorBehaviour : MonoBehaviour
 {
     [SerializeField] private bool doorOpenFlag;
+    private bool prevDoorOpenFlag;
 
     [SerializeField] private int currentDoorStatus = 0;
     [SerializeField] private int doorStatusToOpen = 0;
-
-    [SerializeField] private GameObject leftDoor;
-    [SerializeField] 
-    private GameObject rightDoor;
     [SerializeField] public GameObject doorCollider;
 
     [SerializeField]
     private Animator animator = null;
-    private DoorTransition doorTransition;
 
     [field: SerializeField] public List<string> triggeredElements = new List<string>();
+
+    public bool openFinished = true;
+    public bool openingDoor = false;
+    public bool closedFinished = true;
+    public bool closingDoor = false;
 
 
     private void Start()
@@ -27,10 +29,35 @@ public class doorBehaviour : MonoBehaviour
         animator = GetComponent<Animator>();
         if (doorOpenFlag == true) {
             OpenDoor();
+        } else CloseDoor();
+
+        Debug.Log($"Door status: {currentDoorStatus}");
+    }
+
+    void Update(){
+        if(prevDoorOpenFlag != doorOpenFlag){
+            prevDoorOpenFlag = doorOpenFlag;
+            if(doorOpenFlag && openFinished && !openingDoor && closedFinished && !closingDoor){
+                OpenDoor();
+            }
+            else if(!doorOpenFlag && openFinished && !openingDoor && closedFinished && !closingDoor){
+                CloseDoor();
+            }
+        }
+        
+        if(IsAnimationComplete("DoorOpen")) {
+            openFinished = true;
+            openingDoor = false;
+        }
+        if(IsAnimationComplete("DoorClose")){
+            closedFinished = true;
+            closingDoor = false;
         }
 
-        TryGetComponent<DoorTransition>(out doorTransition);
-        Debug.Log($"Door status: {currentDoorStatus}");
+        if(currentDoorStatus >= doorStatusToOpen && !doorOpenFlag) doorOpenFlag = true;
+        if(currentDoorStatus < doorStatusToOpen && doorOpenFlag)doorOpenFlag = false;
+
+        
     }
 
     public void IncreaseDoorStatus(Trackable trackedValues){
@@ -38,25 +65,32 @@ public class doorBehaviour : MonoBehaviour
         if(!IsTracked(trackedValues.UniqueID)){
             triggeredElements.Add(trackedValues.UniqueID);
             currentDoorStatus = triggeredElements.Count;
-            if(currentDoorStatus >= doorStatusToOpen && !doorOpenFlag){
-                OpenDoor();
-            }
+            
 
         }
     }
        
-
     public void DecreaseDoorStatus(Trackable trackedValues){
         if(trackedValues == null) return;
         if(IsTracked(trackedValues.UniqueID)){
             triggeredElements.Remove(trackedValues.UniqueID);
             currentDoorStatus = triggeredElements.Count;
-            if(currentDoorStatus < doorStatusToOpen && doorOpenFlag){
-                CloseDoor();
-            }
+            
             
         }
-        
+    }
+
+    private bool IsAnimationComplete(string stateName)
+    {
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        Debug.Log($"AnimationName {stateName} ? {stateInfo.IsName(stateName)}");
+        return stateInfo.IsName(stateName) && stateInfo.normalizedTime >= 1.0f;
+    }
+
+     public bool IsPlayingAnimation(string animationName)
+    {
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        return stateInfo.IsName(animationName);
     }
 
     private bool IsTracked(string id){
@@ -67,8 +101,8 @@ public class doorBehaviour : MonoBehaviour
     {
         animator.Play("DoorOpen", 0, 0.0f);
         Debug.Log($"{name} door opened");
-        //leftDoor.SetActive(false);
-        //rightDoor.SetActive(false);
+        openingDoor = true;
+        openFinished = false;
         doorOpenFlag = true;
         doorCollider.GetComponent<Collider>().enabled = true;
     }
@@ -77,8 +111,8 @@ public class doorBehaviour : MonoBehaviour
     {
         animator.Play("DoorClose", 0, 0.0f);
         Debug.Log($"{name} door closed");
-        //leftDoor.SetActive(true);
-        //rightDoor.SetActive(true);
+        closingDoor = true;
+        closedFinished = false;
         doorOpenFlag = false;
         doorCollider.GetComponent<Collider>().enabled = false;
     }
