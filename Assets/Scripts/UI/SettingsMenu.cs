c7f1132d0156be7ce2e0ff8f3c3c60327b512647
Christopher Bridges <162894664+Zoltikrys@users.noreply.github.com>
2025-01-10 17:31:56 +0000

    Resolution settings functionality

--- Assets/Scripts/UI/SettingsMenu.cs
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -3,9 +3,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
+    //audio mixers for sound
+    /*public AudioMixer masterMixer;
+    public AudioMixer musicMixer;
+    public AudioMixer sfxMixer;*/
+
+    public TMPro.TMP_Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> resolutionOptions = new List<string>();
+
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string resolutionOption = resolutions[i].width + "x" + resolutions[i].height;
+            resolutionOptions.Add(resolutionOption);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i; //gets current resolution
+            }
+        }
+
+        resolutionDropdown.AddOptions(resolutionOptions);
+        resolutionDropdown.value = currentResolutionIndex;//automatically sets current resolution
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
     public void SetMasterVolume(float MasterVolume)
     {
         throw new NotImplementedException();
@@ -27,5 +70,11 @@ public class SettingsMenu : MonoBehaviour
     {
         QualitySettings.SetQualityLevel(qualityIndex);
 
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Debug.Log("Toggle fullscreen");
+        Screen.fullScreen = isFullscreen; //won't work outside of a build
     }
 }
