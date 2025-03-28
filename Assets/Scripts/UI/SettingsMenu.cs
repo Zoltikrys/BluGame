2454144aec2a56639af1bc7c5eaffb2763b93c3d
Christopher Bridges <162894664+Zoltikrys@users.noreply.github.com>
2025-03-28 16:17:04 +0000

    Commented out things for the editor that prevented me from building, integrated player preferences into settings menu

--- Assets/Scripts/UI/SettingsMenu.cs
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -4,105 +4,108 @@ using UnityEngine.Audio;
 using UnityEngine.EventSystems;
 using System.Linq;
 using UnityEngine.UI;
+using TMPro;
 
 public class SettingsMenu : MonoBehaviour
 {
     [SerializeField] private GameObject settingsMenuFirst;
 
-
-
-    //audio mixers for sound
     public AudioMixer mainMixer;
-
-    public TMPro.TMP_Dropdown resolutionDropdown;
-
+    public TMP_Dropdown resolutionDropdown;
     public Toggle fullscreenToggle;
+    public TMP_Dropdown qualityDropdown;
 
-    List<Resolution> resolutions = new List<Resolution>();
-    Resolution fallbackResolution;
+    private List<Resolution> resolutions = new List<Resolution>();
 
-    //resolution
     private void Start()
     {
-        fallbackResolution.width = 1920;
-        fallbackResolution.height = 1080;
+        LoadSettings();
+    }
 
-        if (Screen.fullScreen)
-        {
-            fullscreenToggle.isOn = true;
-        }
-        else
-        {
-            fullscreenToggle.isOn = false;
-        }
+    private void LoadSettings()
+    {
+        // Load Fullscreen
+        bool isFullscreen = PlayerPrefs.GetInt("Fullscreen", Screen.fullScreen ? 1 : 0) == 1;
+        Screen.fullScreen = isFullscreen;
+        fullscreenToggle.isOn = isFullscreen;
 
-        
-
-        //resolutions = Screen.resolutions.ToList();
+        // Load Resolution
         resolutions = Screen.resolutions.Where(resolution => resolution.refreshRateRatio.value == 60).ToList();
-
-        resolutionDropdown.ClearOptions(); //clears options
-
+        resolutionDropdown.ClearOptions();
         List<string> resolutionOptions = new List<string>();
 
-
         int currentResolutionIndex = 0;
-        for (int i = 0; i < resolutions.Count; i++) //populating list with available options dependant on monitor
+        for (int i = 0; i < resolutions.Count; i++)
         {
-            string resolutionOption = resolutions[i].width + "x" + resolutions[i].height; //.ToString();
+            string resolutionOption = resolutions[i].width + "x" + resolutions[i].height;
             resolutionOptions.Add(resolutionOption);
 
-            if (resolutions[i].width == Screen.currentResolution.width &&
-                resolutions[i].height == Screen.currentResolution.height)
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
             {
-                currentResolutionIndex = i; //gets current resolution
+                currentResolutionIndex = i;
             }
         }
 
         resolutionDropdown.AddOptions(resolutionOptions);
-        resolutionDropdown.value = currentResolutionIndex;//automatically sets current resolution
+        int savedResolutionIndex = PlayerPrefs.GetInt("Resolution", currentResolutionIndex);
+        resolutionDropdown.value = savedResolutionIndex;
         resolutionDropdown.RefreshShownValue();
+        SetResolution(savedResolutionIndex);
+
+        // Load Quality
+        int qualityIndex = PlayerPrefs.GetInt("Quality", QualitySettings.GetQualityLevel());
+        QualitySettings.SetQualityLevel(qualityIndex);
+        qualityDropdown.value = qualityIndex;
+        qualityDropdown.RefreshShownValue();
+
+        // Load Audio Levels
+        float masterVolume = PlayerPrefs.GetFloat("MasterVolume", 0);
+        float musicVolume = PlayerPrefs.GetFloat("MusicVolume", 0);
+        float sfxVolume = PlayerPrefs.GetFloat("SFXVolume", 0);
+
+        mainMixer.SetFloat("masterVolume", masterVolume);
+        mainMixer.SetFloat("musicVolume", musicVolume);
+        mainMixer.SetFloat("soundFXVolume", sfxVolume);
     }
 
-    public void SetResolution (int resolutionIndex)
+    public void SetResolution(int resolutionIndex)
     {
-        if(resolutions.Count == 0){
-            Screen.SetResolution(1920, 1080, Screen.fullScreen);
-        }
-        else{
+        if (resolutions.Count > 0)
+        {
             Resolution resolution = resolutions[resolutionIndex];
             Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+            PlayerPrefs.SetInt("Resolution", resolutionIndex);
         }
     }
 
-    //volume
     public void SetMasterVolume(float masterVolume)
     {
         mainMixer.SetFloat("masterVolume", masterVolume);
+        PlayerPrefs.SetFloat("MasterVolume", masterVolume);
     }
 
     public void SetMusicVolume(float musicVolume)
     {
         mainMixer.SetFloat("musicVolume", musicVolume);
+        PlayerPrefs.SetFloat("MusicVolume", musicVolume);
     }
 
-    public void SFXVolume(float soundFXVolume)
+    public void SetSFXVolume(float soundFXVolume)
     {
         mainMixer.SetFloat("soundFXVolume", soundFXVolume);
+        PlayerPrefs.SetFloat("SFXVolume", soundFXVolume);
     }
 
-    //graphics quality
     public void SetQuality(int qualityIndex)
     {
         QualitySettings.SetQualityLevel(qualityIndex);
-
+        PlayerPrefs.SetInt("Quality", qualityIndex);
     }
 
-    //fullscreen
     public void SetFullscreen(bool isFullscreen)
     {
-        Debug.Log("Toggle fullscreen");
-        Screen.fullScreen = isFullscreen; //won't work outside of a build
+        Screen.fullScreen = isFullscreen;
+        PlayerPrefs.SetInt("Fullscreen", isFullscreen ? 1 : 0);
     }
 
     public void EnteringMenu()
