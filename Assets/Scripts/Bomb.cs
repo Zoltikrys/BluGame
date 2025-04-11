923218699c69d7b9215e1a9a4f27c4a8df094088
Christopher Bridges <162894664+Zoltikrys@users.noreply.github.com>
2025-04-11 17:05:02 +0100

    Bomb and landmine prefabs

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -6,46 +6,66 @@ public class Bomb : MonoBehaviour
 {
     [SerializeField] private bool isTriggered = false;
     [SerializeField] private bool canExplode = false;
+    [SerializeField] private bool hasExploded = false;
 
-    public float detonatePoint;
-    public float detonateTimer = 0f;
+    [SerializeField] private KnockbackEffect knockback;
+
+    public float detonateTime = 4f; //4 Seconds until detonation is default
+    [SerializeField] public float detonateTimer = 0f;
+
+    [SerializeField] private float explosionAnimDone = 5f; //don't know how long this will be yet
+    [SerializeField] private float explosionDeltaTime = 0f;
+
     [SerializeField] Collider explosionArea;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
         if (isTriggered)
         {
+            //play fuse vfx
             detonateTimer += Time.deltaTime;
-            if (detonateTimer >= detonatePoint)
+            if (detonateTimer >= detonateTime)
             {
                 canExplode = true;
             }
 
         }
 
+        if (canExplode)
+        {
+            //Play animation
+            //play explosion vfx
+            //hasExploded = true;
+            explosionDeltaTime += Time.deltaTime;
+
+            /*
+            if (explosionDeltaTime >= explosionAnimDone)
+            {
+                hasExploded = true;
+                //delete self
+            }
+            */
+        }
     }
 
     public void OnTriggerStay(Collider other)
     {
         if (canExplode)
         {
-            //Play animation
-            if (other.CompareTag("Player"))
+            if (!hasExploded) //So damange only happens once
             {
-                //damage player
+                if (other.CompareTag("Player"))
+                {
+                    Debug.Log("Player in explosion radius");
+                    //damage player
+
+                    //knockback
+                    knockback.ApplyRadialKnockback(other.gameObject.GetComponent<CharacterController>(), transform.position);
+                    hasExploded = true; //remove later
+                }
             }
 
-
-
-
-            //Delete self
         }
     }
 
