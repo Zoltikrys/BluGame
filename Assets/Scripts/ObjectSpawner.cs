addf42997e14246c23260af0818d21c0689c637b
Christopher Bridges <162894664+Zoltikrys@users.noreply.github.com>
2025-04-19 16:37:50 +0100

    Demo level room 2

--- Assets/Scripts/ObjectSpawner.cs
+++ Assets/Scripts/ObjectSpawner.cs
@@ -7,11 +7,33 @@ public class ObjectSpawner : MonoBehaviour
     public List<GameObject> PrefabsToSpawn = new List<GameObject>();
     public GameObject objectContainer;
     public float spawnEveryXSeconds = 1.0f;
+    public float spawnOffsetTime = 0f;
+    public bool canStart = false;
+    public bool hasStarted = false;
 
     void Start()
     {
-        InvokeRepeating(nameof(SpawnObject), 0.0f, spawnEveryXSeconds);
+        if (canStart)
+        {
+            InvokeRepeating(nameof(SpawnObject), 0.0f, spawnEveryXSeconds);
+            hasStarted = true;
+        }
     }
+
+    private void Update()
+    {
+        if (spawnOffsetTime > 0f)
+        {
+            spawnOffsetTime = spawnOffsetTime - Time.deltaTime;
+        }
+
+        if (spawnOffsetTime <= 0f && !hasStarted)
+        {
+            canStart = true;
+            Start();
+        }
+    }
+
 
     private void SpawnObject(){
         if(PrefabsToSpawn.Count > 0){
