71af8e2a5cb9b327aa7a635b7cc4d875c8e50274
Christopher Bridges <162894664+Zoltikrys@users.noreply.github.com>
2025-05-18 21:30:06 +0100

    Added boss functionality, defeating boss now also goes to end cutscene

--- Assets/Scripts/AI/Boss.cs
+++ Assets/Scripts/AI/Boss.cs
@@ -7,6 +7,7 @@ public class Boss : MonoBehaviour
     public GameObject blockingwall;
     public ParticleSystem smokeParticle1;
     public ParticleSystem smokeParticle2;
+    public doorBehaviour endDoor;
 
     public void Death(){
         Destroy(grinder);
@@ -14,5 +15,6 @@ public class Boss : MonoBehaviour
         Destroy(blockingwall);
         smokeParticle1.Stop();
         smokeParticle2.Stop();
+        endDoor.IncreaseDoorStatus();
     }
 }
